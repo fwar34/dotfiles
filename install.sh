e2e72e999a2e804ab4196c2620a8eac7ecadac94
fwar34 <fwar34@126.com>
2020-12-15 21:27:41 +0800

    add pip.conf

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #.config
-for file in $(ls .config)
-do
+if [[ ! -d ~/.config ]]; then
+    mkdir -p ~/mine
+fi
+
+for file in $(ls .config); do
     ln -sf $PWD/.config/$file $HOME/.config/$(basename $file)
 done
+
+#pip
+ln -sf $PWD/.pip ~/.pip
 
 echo complete
