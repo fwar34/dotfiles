461c006c95eb66d8aa6df26faebce6fe950e3400
fwar34 <fwar34@126.com>
2020-12-16 11:11:21 +0800

    update

--- install.sh
+++ install.sh
@@ -5,11 +5,21 @@ if [[ ! -d ~/.config ]]; then
     mkdir -p ~/mine
 fi
 
+#awesome wm
 for file in $(ls .config); do
     ln -sf $PWD/.config/$file $HOME/.config/$(basename $file)
 done
 
 #pip
 ln -sf $PWD/.pip ~/.pip
+
+#xmonad wm
+#http://www.ruanyifeng.com/blog/2017/07/xmonad.html
+#sudo apt install xmonad xmobar dmenu
+if [[ ! -d ~/.xmonad ]]; then
+    mkdir -p ~/xmonad
+fi
+ln -sf $PWD/.xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
+ln -sf $PWD/.xmobarrc $HOME/.xmobarrc
 
 echo complete
