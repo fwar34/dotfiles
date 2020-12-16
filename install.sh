feee5c0b5fc7005f11613a7cc2576a434ec38f0f
fwar34 <fwar34@126.com>
2020-12-16 11:25:42 +0800

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [[ ! -d ~/.config ]]; then
 fi
 
 #awesome wm
+#sudo apt install awesome awesome-extra
 for file in $(ls .config); do
     ln -sf $PWD/.config/$file $HOME/.config/$(basename $file)
 done
