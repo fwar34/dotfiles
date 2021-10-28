3155eecf43e7db583dd4f8822bf0cf0c99596ae9
fwar34 <fwar34@126.com>
2021-10-28 16:51:03 +0800

    update

--- .config/awesome/themes/multicolor/theme-personal.lua
+++ .config/awesome/themes/multicolor/theme-personal.lua
@@ -16,7 +16,8 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
-theme.wallpaper                                 = theme.confdir .. "/wall.png"
+-- theme.wallpaper                                 = theme.confdir .. "/wall.png"
+theme.wallpaper                                 = theme.confdir .. "./wall.png"
 -- theme.font                                      = "Terminus 8"
 theme.font                                      = "Noto Sans Regular 10"
 theme.menu_bg_normal                            = "#000000"
