6135d23c7512ea1012a72199d0dd744a926e1f84
fwar34 <fwar34@126.com>
2022-10-08 19:53:19 +0800

    update

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ fi
 git submodule update --init --recursive
 
 #Xresource
-ln -sf $PWD/.Xresources ~/.Xresources
+# ln -sf $PWD/.Xresources ~/.Xresources
+cp $PWD/.Xresources ~/.Xresources
 xrdb ~/.Xresources
 
 # environment
@@ -17,16 +18,20 @@ ln -sf $PWD/.pam_environment ~/.pam_environment
 #awesome wm ranger
 #sudo apt install awesome awesome-extra
 for file in $(ls .config); do
-    if [[ ${file} != "lemonade.toml" ]]; then
+    if [[ ${file} != "lemonade.toml" ]] && [[ ${file} != "rofi" ]] && [[ ${file} != "alacritty" ]]; then
         ln -sf $PWD/.config/$file $HOME/.config/$(basename $file)
         echo ln -sf $PWD/.config/$file $HOME/.config/$(basename $file)
     fi
 done
 
-if [[ -f ~/.config/lemonade.toml  ]]; then
-    mv ~/.config/lemonade.toml ~/.config/lemonade.toml.bak
-fi
-cp $PWD/lemonade.toml ~/.config/lemonade.toml
+cp -r $PWD/.config/rofi ~/.config/
+cp -r $PWD/.config/kitty ~/.config/
+cp -r $PWD/.config/alacritty ~/.config/
+
+# if [[ -f ~/.config/lemonade.toml  ]]; then
+#     mv ~/.config/lemonade.toml ~/.config/lemonade.toml.bak
+# fi
+# cp $PWD/lemonade.toml ~/.config/lemonade.toml
 
 #pip
 ln -sf $PWD/.pip ~/.pip
